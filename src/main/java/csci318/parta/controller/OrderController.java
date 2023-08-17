ebe18c6a1da566f9999d7a4d49776c66f8675631
KatsuyaSaito0 <113318609+KatsuyaSaito0@users.noreply.github.com>
2023-08-18 07:43:53 +1000

    Update OrderController.java

--- src/main/java/csci318/parta/controller/OrderController.java
+++ src/main/java/csci318/parta/controller/OrderController.java
@@ -1,10 +1,10 @@
 package csci318.parta.controller;
 
-import csci318.parta.repository.OrderRepository;
-import csci318.parta.model.Order;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+import org.example.model.Order;
+import org.example.repository.OrderRepository;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -12,17 +12,19 @@ public class OrderController {
 
     private final OrderRepository orderRepository;
 
-    OrderController(OrderRepository orderRepository) {
+    public OrderController(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
     }
 
+    //@GetMapping is for GET requests
     @GetMapping("/order")
     List<Order> findAllOrders() {
         return orderRepository.findAll();
     }
 
+    //this is used to find order id
     @GetMapping("/order/{id}")
-    Order getOrderById(@PathVariable Long id) {
+    Order findOrderById(@PathVariable Long id) {
         return orderRepository.findById(id)
                 .orElseThrow(RuntimeException::new);
     }
@@ -30,6 +32,15 @@ public class OrderController {
     @PostMapping("/order")
     Order createOrder(@RequestBody Order newOrder) {
         return orderRepository.save(newOrder);
+    }
+
+    //put order by id
+    @PutMapping("/order/{id}")
+    Order updateOrderName(@PathVariable Long id, @RequestBody Order order) {
+        Order order1 = orderRepository.findById(id)
+                .orElseThrow(RuntimeException::new);
+        order1.setName(order.getName());
+        return orderRepository.save(order1);
     }
 
 }
