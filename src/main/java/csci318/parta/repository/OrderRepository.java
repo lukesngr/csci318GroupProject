31828f14ec5d8be068e11ff998b9f12b5deea13f
KatsuyaSaito0 <113318609+KatsuyaSaito0@users.noreply.github.com>
2023-08-18 08:20:58 +1000

    Update OrderRepository.java

--- src/main/java/csci318/parta/repository/OrderRepository.java
+++ src/main/java/csci318/parta/repository/OrderRepository.java
@@ -1,9 +1,8 @@
 package csci318.parta.repository;
 
-import csci318.parta.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+import csci318.parta.model.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
